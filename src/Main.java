770803661d2803258302f3d62061b94dbc80c04c
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-02 22:59:53 -0300

    feat: versão inicial jogo de par ou impar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random gerador = new Random();
+
+        System.out.println("Par ou Impar?\n" +
+                "0 - Impar\n" +
+                "1 - Par");
+        int parImpar = Integer.parseInt(scanner.nextLine());
+        System.out.print("Qual seu numero de 0 a 5? ");
+        int numeroMaquina = gerador.nextInt(6);
+        int numeroPlayer = Integer.parseInt(scanner.nextLine());
+
+        boolean par = (numeroPlayer+numeroMaquina) % 2 == 0;
+
+        if ((par && parImpar > 0) || (!par && parImpar == 0)){
+            System.out.println("Parabens você ganhou");
+        } else {
+            System.out.println("Infelizmente você perdeu");
+        }
+
     }
 }
